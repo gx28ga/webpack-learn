43c8843c89c2e482e1b28ca0c24413ef428f1325
randy <gx28ga@hotmail.com>
2020-04-07 00:55:26 +0800

    init

---
+++ src/search.js
@@ -0,0 +1,12 @@
+"use strict";
+import React from "react";
+import ReactDOM from "react-dom";
+class Search extends React.Component{
+    render() {
+        return (
+            <div>search text</div>
+        )
+    }
+}
+
+ReactDOM.render(<Search />, document.querySelector("#root"));
